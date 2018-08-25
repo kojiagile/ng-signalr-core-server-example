316e0a7bee8c0b6610969c614658a562dd5c0487
koji nishimoto <koji.nishimoto@orica.com>
2018-08-25 14:33:24 +1000

    Initial commit

---
+++ ng-signalr-core/ChatHub.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ng_signalr_core
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", 
+                new ChatMessage { UserName = user, Message = message });
+        }
+
+        public async Task SendMessageForComment(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessageWithComment",
+                new ChatMessageWithComment { UserName = user, Message = message, Comment = "Great job!" });
+        }
+
+        public ChannelReader<int> Counter(int count, int delay)
+        {
+            var channel = Channel.CreateUnbounded<int>();
+
+            // We don't want to await WriteItems, otherwise we'd end up waiting 
+            // for all the items to be written before returning the channel back to
+            // the client.
+            _ = WriteItems(channel.Writer, count, delay);
+
+            return channel.Reader;
+        }
+
+        private async Task WriteItems(ChannelWriter<int> writer, int count, int delay)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                await writer.WriteAsync(i);
+                await Task.Delay(delay);
+            }
+
+            writer.TryComplete();
+        }
+    }
+    
+    public class ChatMessage
+    {
+        public string UserName { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class ChatMessageWithComment : ChatMessage
+    {
+        public string Comment { get; set; }
+    }
+}
---
+++ ng-signalr-core/Startup.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ng_signalr_core
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Allow any 
+            services.AddCors(options =>
+            {
+                // Make sure the CORS middleware is ahead of SignalR.
+                // TODO: Set up policy correctly. 
+                // This code is for testing only. 
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.WithOrigins("http://localhost:4200")
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                );
+            });
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors("CorsPolicy");
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("/chatHub");
+            });
+
+            app.UseMvc();
+        }
+    }
+}
